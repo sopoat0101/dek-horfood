11e128988bf4d6938ab859265b49af53674498e8
60070101 <60070101@kmitl.ac.th>
2017-11-07 20:28:30 +0700

    Add nevbar in web

--- web/index.php
+++ web/index.php
@@ -1,1 +1,13 @@
-<?php echo 'Hello ITF' ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dek-Horfood</title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <div class="nev">
+        <a href="#">Sing in|Sing up</a>
+    </div>
+  </body>
+</html>
