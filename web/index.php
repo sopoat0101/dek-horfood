3307b8197cb7acea30d056c4ecbe4fb1ede777c1
60070101 <60070101@kmitl.ac.th>
2017-11-07 21:03:15 +0700

    Add button to web

--- web/index.php
+++ web/index.php
@@ -3,11 +3,26 @@
   <head>
     <meta charset="utf-8">
     <title>Dek-Horfood</title>
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="../css/master.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
-    <div class="nev">
-        <a href="#">Sing in|Sing up</a>
+    <div class="nevbar">
+      <div id="center">
+        <a class="text-white" href="#">Sign in</a>
+        |
+        <a class="text-white" href="#">Sign up</a>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <button type="button" class="btn btn-warning">ค้นหาจากวัตถุดิบ</button>
+        </div>
+        <div class="col">
+          <button type="button" class="btn btn-warning">ค้านหาจากชื่อเมนู</button>
+        </div>
+      </div>
     </div>
   </body>
 </html>
