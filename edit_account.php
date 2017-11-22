e95099e8ebcac50cc50ee3c3faa11a6158a72a6a
Supakit Theanthunyakit <mr.pokiloli@gmail.com>
2017-11-22 14:55:54 +0700

    wowo
    add input in account

---
+++ edit_account.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dek-Horfood-Edit</title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="" href="img/icon.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+    <div class="nev">
+        <div class="innev">
+            <a href="index.php"><div id="logo"><img id="inlogo" src="img/logo.png"></div></a>
+            <a href="login.php"><div id="addmenu">Addmenu</div></a>
+            <a href="infomation.php" target="_blank"><div id="info">Infomation</div></a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <form action="#">
+            <div class="edit_user">
+                <div class="row justify-content-center">
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">Name :</h5>
+                        <input class="getinput edit_user" id="username" type="text" name="username" placeholder="Supagit">
+                    </div>
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">Name :</h5>
+                        <input class="getinput edit_user" id="username" type="text" name="username" placeholder="Supagit">
+                    </div>
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">Surname :</h5>
+                        <input class="getinput edit_user" id="username" type="text" name="username" placeholder="Supagit">
+                    </div>
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">E-mail :</h5>
+                        <input class="getinput edit_user" id="username" type="text" name="username" placeholder="Supagit@hub.com">
+                    </div>
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">Password :</h5>
+                        <input class="getinput edit_user" id="username" type="Password" name="username" placeholder="Supagit">
+                    </div>
+                    <div class="row col-8 justify-content-center getinput">
+                        <h5 class="getinput">New Password :</h5>
+                        <input class="getinput edit_user" id="username" type="Password" name="username" placeholder="Supagit">
+                    </div>
+                </div>
+                <input class="btn_save" type="submit" value="save">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
