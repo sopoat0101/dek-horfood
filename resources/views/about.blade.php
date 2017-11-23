c36f6f918ec9f169632de6b7502a04424ce81bb0
60070101 <60070101@kmitl.ac.th>
2017-11-23 07:30:54 +0700

    add about us and pic

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,7 @@
 	<div class="row justify-content-center">
 		<div class="col-sm-4">
 			<div style="text-align: center; padding: 2%;">
-				<img class="pro" style="background-image: url(img/egg.jpg);">
+				<img class="pro" style="background-image: url(img/nine.jpg);">
 				<h4>Sopoat Iamcharoen</h4>
 			</div>
 		</div>
@@ -34,12 +34,8 @@
 					<div class="col-sm-5">KMITL Infomation technology</div>
 				</div>
 				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
+					<div class="col-sm-3">Position:</div>
+					<div class="col-sm-5">Front-end</div>
 				</div>
 			</div>
 		</div>
@@ -48,60 +44,52 @@
 			<div class="center">
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Name:</div>
-					<div class="col-sm-5">Sopoat Iamcharoen</div>
+					<div class="col-sm-5">Pachara Sriyodwieng</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">ID:</div>
-					<div class="col-sm-5">60070101</div>
+					<div class="col-sm-5">60070057</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Studen:</div>
 					<div class="col-sm-5">KMITL Infomation technology</div>
 				</div>
 				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
+					<div class="col-sm-3">Position:</div>
+					<div class="col-sm-5">Front-end</div>
 				</div>
 			</div>
 		</div>
 		<div class="col-sm-4">
 			<div style="text-align: center; padding: 2%;">
-				<img class="pro" style="background-image: url(img/egg.jpg);">
-				<h4>Sopoat Iamcharoen</h4>
+				<img class="pro" style="background-image: url(img/boom.jpg);">
+				<h4>Pachara Sriyodwieng</h4>
 			</div>
 		</div>
 		<!--2-->
 		<div class="col-sm-4">
 			<div style="text-align: center; padding: 2%;">
-				<img class="pro" style="background-image: url(img/egg.jpg);">
-				<h4>Sopoat Iamcharoen</h4>
+				<img class="pro" style="background-image: url(img/pok.jpg);">
+				<h4>Supakit Theanthunyakit</h4>
 			</div>
 		</div>
 		<div id="do" class="col-sm-8">
 			<div class="center">
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Name:</div>
-					<div class="col-sm-5">Sopoat Iamcharoen</div>
+					<div class="col-sm-5">Supakit Theanthunyakit </div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">ID:</div>
-					<div class="col-sm-5">60070101</div>
+					<div class="col-sm-5">60070098</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Studen:</div>
 					<div class="col-sm-5">KMITL Infomation technology</div>
 				</div>
 				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
+					<div class="col-sm-3">Position:</div>
+					<div class="col-sm-5">Front-end</div>
 				</div>
 			</div>
 		</div>
@@ -109,59 +97,51 @@
 			<div class="center">
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Name:</div>
-					<div class="col-sm-5">Sopoat Iamcharoen</div>
+					<div class="col-sm-5">Wassapol Pungjap</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">ID:</div>
-					<div class="col-sm-5">60070101</div>
+					<div class="col-sm-5">60070088</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Studen:</div>
 					<div class="col-sm-5">KMITL Infomation technology</div>
 				</div>
 				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
+					<div class="col-sm-3">Position:</div>
+					<div class="col-sm-5">Back-end</div>
 				</div>
 			</div>
 		</div>
 		<div class="col-sm-4">
 			<div style="text-align: center; padding: 2%;">
-				<img class="pro" style="background-image: url(img/egg.jpg);">
-				<h4>Sopoat Iamcharoen</h4>
+				<img class="pro" style="background-image: url(img/pp.jpg);">
+				<h4>Wassapol Pungjap</h4>
 			</div>
 		</div>
 		<div class="col-sm-4">
 			<div style="text-align: center; padding: 2%;">
-				<img class="pro" style="background-image: url(img/egg.jpg);">
-				<h4>Sopoat Iamcharoen</h4>
+				<img class="pro" style="background-image: url(img/pop.jpg);">
+				<h4>Thitichote Chaimuang</h4>
 			</div>
 		</div>
 		<div id="do" class="col-sm-8">
 			<div class="center">
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Name:</div>
-					<div class="col-sm-5">Sopoat Iamcharoen</div>
+					<div class="col-sm-5">Thitichote Chaimuang</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">ID:</div>
-					<div class="col-sm-5">60070101</div>
+					<div class="col-sm-5">60070019</div>
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-3">Studen:</div>
 					<div class="col-sm-5">KMITL Infomation technology</div>
 				</div>
 				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-sm-3">Studen:</div>
-					<div class="col-sm-5">KMITL Infomation technology</div>
+					<div class="col-sm-3">Position:</div>
+					<div class="col-sm-5">Back-end</div>
 				</div>
 			</div>
 		</div>
